ba5c1a07a30e075517e2b88fa73742e1d617556c
anhdev99 <90453773+anhdev99@users.noreply.github.com>
2023-05-24 16:30:11 +0700

    [update]

--- Areas/Modules.Account/App/main.js
+++ Areas/Modules.Account/App/main.js
@@ -3,12 +3,9 @@ import VueRouter from 'vue-router';
 import App from './App.vue'
 import router from "./router/index";
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-// import './assets/styles/style.css'
 
 import store from "./state/store";
-import "./design/app.scss";
+import "./design/index.scss";
 
 Vue.component('default-layout', () => import(/* webpackChunkName: "layout-container" */ './components/layout/DefaultLayout.vue'));
 Vue.use(VueRouter);
--- Areas/Modules.Account/App/pages/home/index.vue
+++ Areas/Modules.Account/App/pages/home/index.vue
@@ -21,6 +21,30 @@
         </b-dropdown>
       </div>
     </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Rounded buttons</h4>
+            <p class="card-title-desc">
+              Use
+              <code>pill</code> for button round border.
+            </p>
+            <div class="button-items">
+              <b-button pill variant="primary">Primary</b-button>
+              <b-button pill variant="secondary">Secondary</b-button>
+              <b-button pill variant="success">Success</b-button>
+              <b-button pill variant="info">Info</b-button>
+              <b-button pill variant="warning">Warning</b-button>
+              <b-button pill variant="danger">Danger</b-button>
+              <b-button pill variant="dark">Dark</b-button>
+              <b-button pill variant="link">Link</b-button>
+              <b-button pill variant="light">Light</b-button>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
     <router-link to="Account/dang-nhap"> Dang Nhap</router-link>
   </default-layout>
 </template>
